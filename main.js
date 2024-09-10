e42e432b89aabc11ac0a6dd971c94564f5a27d5a
AlbertProfeIronhack <157632155+AlbertProfeIronhack@users.noreply.github.com>
2024-09-10 18:45:26 +0200

    prueba pa Netlify

---
+++ main.js
@@ -0,0 +1,8 @@
+function arranca(){
+    // .style modifica el CSS
+    document.getElementById("bolita").style.animationPlayState="running";
+}
+
+function para(){
+    document.getElementById("bolita").style.animationPlayState="paused";
+}
